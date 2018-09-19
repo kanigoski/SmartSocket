70c600462d3912232b76ad69cbe0be81c4ddf029
TheVdropSupreme <43391762+TheVdropSupreme@users.noreply.github.com>
2018-09-18 21:42:06 -0300

    Add files via upload

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -16,25 +16,25 @@ export class TableComponent implements OnInit{
     public tableData2: TableData;
     ngOnInit(){
         this.tableData1 = {
-            headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+            headerRow: [ 'ID', 'Referência', 'Média/Hora', 'Total/Hora', 'Valor Estimado'],
             dataRows: [
-                ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-                ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-                ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-                ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-                ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-                ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+                ['1', 'Aparelho de Som', '0.002', '0.02', 'R$5,10'],
+                ['2', 'Frigobar', '0.007', '0.07', 'R$8,60'],
+                ['3', 'Torradeira', '0.005', '0.50', 'R$12,50'],
+                ['4', 'Tv', '0.09', '0.090', 'R$20,12'],
+                ['5', 'Geladeira', '0.20', '0.0020', 'R$15,50'],
+                ['6', 'Microondas', '0.41', '0.0047', 'R$5,23']
             ]
         };
         this.tableData2 = {
-            headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
+            headerRow: [ 'ID', 'Referência',  'Média/Hora', 'Total/Hora', 'Valor Estimado' ],
             dataRows: [
-                ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
-                ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-                ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
-                ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
-                ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
-                ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
+                ['1', 'Freezer','0.54', '0.0540', 'R$13,33' ],
+                ['2', 'Cafeteira', '0.30', '0.003', 'R$0,25'],
+                ['3', 'Filtro de Ar', '0.12', '0.120', 'R$1,23' ],
+                ['4', 'Aquecedor', '0.50', '0.560', 'R$4,56' ],
+                ['5', 'Fritadiera', '0.22', '0.452', 'R$7,89', ],
+                ['6', 'Ar Condicionado', '0.452', '1.259', 'R$5,67' ]
             ]
         };
     }
